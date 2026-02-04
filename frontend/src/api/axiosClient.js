43212cfbf81ed761f5fec91a86f90fbc4a4bb4af
vannhan-1602 <vovannhan160204@gmail.com>
2026-02-04 19:03:44 +0700

    fix frontend

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", // Đường dẫn đến Backend Laravel của bạn
+  baseURL: "https://shopii-backend-latest.onrender.com", //backend
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
