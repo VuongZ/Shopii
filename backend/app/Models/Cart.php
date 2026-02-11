e5e7483fe661c6c47d6304ee8092ca09f106a22e
vannhan-1602 <vovannhan160204@gmail.com>
2026-02-11 08:40:45 +0700

    upload models

--- backend/app/Models/Cart.php
+++ backend/app/Models/Cart.php
@@ -10,8 +10,8 @@ class Cart extends Model
     use HasFactory;
     protected $guarded = [];
 
-    public function items()
-    {
-        return $this->hasMany(CartItem::class);
-    }
+   public function items()
+{
+    return $this->hasMany(CartItem::class, 'cart_id');
+}
 }
