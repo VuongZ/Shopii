53e18f5d119d7992f76ffbbe6bbf7443b7104c4f
HOAITRUNG NGUYEN <trungnguyenhoai375@gmail.com>
2026-02-23 19:28:40 +0700

    xoa icon

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -7,15 +7,33 @@ use App\Models\Product; //
 
 class ProductController extends Controller
 {
-    // Lấy danh sách tất cả sản phẩm
+    // Lấy danh sách sản phẩm
     public function index()
     {
-        return Product::all();
+        return Product::with([
+            'shop',
+            'category',
+            'product_images',
+            'skus'
+        ])->get();
     }
 
     // Lấy chi tiết 1 sản phẩm
     public function show($id)
     {
-        return Product::find($id);
+        $product = Product::with([
+            'shop',
+            'category',
+            'product_images',
+            'skus'
+        ])->find($id);
+
+        if (!$product) {
+            return response()->json([
+                'message' => 'Product not found'
+            ], 404);
+        }
+
+        return $product;
     }
 }
