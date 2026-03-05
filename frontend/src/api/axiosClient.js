c420ae5355d4eb1eba06bdef9136dca71630af90
Huy Vo <youremail@example.com>
2026-03-05 19:00:52 +0700

    pull code

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,35 +1,37 @@
-import axios from "axios";
+import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: "https://shopii-backend-latest.onrender.com/api",
+  // https://shopii-backend-latest.onrender.com/api backend render
+  // http://localhost:8000/api/
+  baseURL: 'https://shopii-backend-latest.onrender.com/api',
   headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
   },
-});
+})
 
 // Tự động thêm Token vào mỗi request nếu đã đăng nhập
 axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("ACCESS_TOKEN");
+  const token = localStorage.getItem('ACCESS_TOKEN')
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`
   }
-  return config;
-});
+  return config
+})
 
 // Xử lý lỗi chung (Ví dụ: Hết hạn token thì tự đăng xuất)
 axiosClient.interceptors.response.use(
   (response) => {
-    return response;
+    return response
   },
   (error) => {
-    const { response } = error;
+    const { response } = error
     if (response && response.status === 401) {
-      localStorage.removeItem("ACCESS_TOKEN");
+      localStorage.removeItem('ACCESS_TOKEN')
       // window.location.reload();
     }
-    throw error;
-  },
-);
+    throw error
+  }
+)
 
-export default axiosClient;
+export default axiosClient
