81ab91aace6e44b896b044317875ac31137c4ed3
Huy Vo <youremail@example.com>
2026-03-16 21:08:00 +0700

    update composer lock

--- backend/config/database.php
+++ backend/config/database.php
@@ -60,7 +60,7 @@ return [
             'engine' => null,
             
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-            PDO::MYSQL_ATTR_SSL_CA => base_path('cacert.pem'),    
+            PDO::MYSQL_ATTR_SSL_CA => base_path(env('MYSQL_ATTR_SSL_CA')),    
             PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, 
             ]) : [],
         ],
