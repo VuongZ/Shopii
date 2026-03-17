9f8d03f2340ce88224000395f9d37bba203343b2
Huy Vo <youremail@example.com>
2026-03-17 20:56:51 +0700

    cap nhat trang register reset forgot

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,21 +1,30 @@
-import axiosClient from "./axiosClient";
+import axiosClient from './axiosClient'
 
 const userApi = {
   login(data) {
-    return axiosClient.post("/login", data);
+    return axiosClient.post('/login', data)
   },
 
   register(data) {
-    return axiosClient.post("/register", data);
+    return axiosClient.post('/register', data)
   },
 
   getProfile() {
-    return axiosClient.get("/user");
+    return axiosClient.get('/user')
   },
 
   logout() {
-    return axiosClient.post("/logout");
+    return axiosClient.post('/logout')
   },
-};
+  forgotPassword(email) {
+    return axiosClient.post('/forgot-password', { email })
+  },
+  ResetPassword(data) {
+    return axiosClient.post('/reset-password', data)
+  },
+  verifyOTP(data) {
+    return axiosClient.post('/verify-otp', data)
+  },
+}
 
-export default userApi;
+export default userApi
