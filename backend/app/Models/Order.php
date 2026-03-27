99b49b76f36384b3520ab5d8375af4c2379a2167
vannhan-1602 <vovannhan160204@gmail.com>
2026-03-27 13:02:03 +0700

    upload

--- backend/app/Models/Order.php
+++ backend/app/Models/Order.php
@@ -19,4 +19,14 @@ class Order extends Model
     {
         return $this->belongsTo(Shop::class);
     }
+
+    public function userAddress()
+    {
+        return $this->belongsTo(UserAddress::class, 'user_address_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
